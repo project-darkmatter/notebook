1a594c74ee5dff9e84820da60cf72c0b5bace2de
tamamu <tamamu.1r1s@gmail.com>
2017-11-14 22:59:09 +0900

    Add extention module which support the formats of Darkmatter Notebook

---
+++ src/extention.lisp
@@ -0,0 +1,8 @@
+(in-package :cl-user)
+(defpackage darkmatter/notebook/extention
+  (:use :cl)
+  (:export :read-as-notebook))
+(in-package :darkmatter/notebook/extention)
+
+(defun read-as-notebook (pathname)
+  nil)
