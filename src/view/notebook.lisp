7b2cca00f00d173c650b888017e493d59585cb92
tamamu <tamamu.1r1s@gmail.com>
2017-11-14 22:57:16 +0900

    Add notebook view

---
+++ src/view/notebook.lisp
@@ -0,0 +1,13 @@
+(in-package :cl-user)
+(defpackage darkmatter/notebook/view/notebook
+  (:use :cl
+        :darkmatter/notebook/view/base)
+  (:export :view.notebook))
+(in-package :darkmatter/notebook/view/notebook)
+
+(let* ((djula:*current-store* *pages-store*)
+       (template (djula:compile-template* "notebook.html")))
+  (defun view.notebook (notebook)
+    (render-html
+      (djula:render-template* template nil
+                              :notebook notebook))))
