eab8c0b81ac7f99a8d46d2244c0ac8831c6cffe7
tamamu <tamamu.1r1s@gmail.com>
2017-12-04 23:59:09 +0900

    Fix test: serialize ipynb-format structure which is converted from cell entities

--- test/ext-ipynb.lisp
+++ test/ext-ipynb.lisp
@@ -2,7 +2,11 @@
 (defpackage darkmatter/notebook/test/extentions/ipynb
   (:use :cl
         :darkmatter/notebook/extentions/ipynb
-        :prove))
+        :prove)
+  (:import-from :darkmatter/notebook/domains/cell
+                :make-note-cell-entity
+                :make-code-cell-entity
+                :make-stream-output-entity))
 (in-package :darkmatter/notebook/test/extentions/ipynb)
 
 
@@ -10,29 +14,26 @@
 
 (plan 2)
 
+(defvar *cells*
+  (list
+    (make-note-cell-entity
+      :format :markdown
+      :source (list "some *markdown*"))
+    (make-code-cell-entity
+      :source "some code"
+      :execution-count 1
+      :collapsed t
+      :autoscroll nil
+      :outputs (list
+                 (make-stream-output-entity
+                   :stream :stdout
+                   :source (list "multiline stream text"))))))
+
 (subtest "Serialize .ipynb"
-  (let ((raw (make-ipynb
-               :metadata '(:signature :hex-digest
-                           :kernel-info (:name "darkmatter")
-                           :language-info (:name "common-lisp"
-                                           :version "X3J13"
-                                           :codemirror-mode "commonlisp"))
-               :nbformat 4
-               :nbformat-minor 0
-               :cells (list (make-cell :type "markdown"
-                                       :metadata nil
-                                       :source '("some *markdown*"))
-                            (make-cell :type "code"
-                                       :execution-count 1
-                                       :metadata '(:collapsed t
-                                                   :autoscroll nil)
-                                       :source '("some code")
-                                       :outputs `(,(make-code-cell-output
-                                                     :type :stream
-                                                     :name "stdout"
-                                                     :text '("multiline stream text"))))))))
-    (is (serialize.ipynb raw)
-        "{\"metadata\":{\"signature\":\"hex-digest\",\"kernel_info\":{\"name\":\"darkmatter\"},\"language_info\":{\"name\":\"common-lisp\",\"version\":\"X3J13\",\"codemirror_mode\":\"commonlisp\"}},\"nbformat\":4,\"nbformat_minor\":0,\"cells\":[{\"cell_type\":\"markdown\",\"metadata\":{},\"source\":[\"some *markdown*\"]},{\"cell_type:\"code\",\"execution_count:1,\"metadata\":{\"collapsed\":True,\"autoscroll\":False},\"source\":[\"some code\"],\"outputs\":[{\"output_type\":\"stream\",\"name\":\"stdout\",\"text\":[\"multiline stream text\"]}]\"\"}]}")))
+  (let* ((ipynb (map-to-ipynb *cells*))
+         (serialized (serialize.ipynb ipynb)))
+    (is serialized
+        "{\"metadata\":{\"signature\":\"hex-digest\",\"kernel_info\":{\"name\":\"darkmatter\"},\"language_info\":{\"name\":\"common-lisp\",\"version\":\"X3J13\",\"codemirror_mode\":\"commonlisp\"}},\"nbformat\":4,\"nbformat_minor\":0,\"cells\":[{\"cell_type\":\"markdown\",\"metadata\":{},\"source\":[\"some *markdown*\"]},{\"cell_type\":\"code\",\"execution_count\":1,\"metadata\":{\"collapsed\":true,\"autoscroll\":false},\"source\":\"some code\",\"outputs\":[{\"output_type\":\"stream\",\"name\":\"stdout\",\"text\":[\"multiline stream text\"]}]}]}")))
 
 (subtest "Deserialize .ipynb"
   )
