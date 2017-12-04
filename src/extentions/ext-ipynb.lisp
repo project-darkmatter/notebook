3786cabf30686ca6ce98488ef8f2dd2ee24376ae
tamamu <tamamu.1r1s@gmail.com>
2017-12-04 23:59:29 +0900

    Implement serialize.ipynb

--- src/extentions/ext-ipynb.lisp
+++ src/extentions/ext-ipynb.lisp
@@ -1,7 +1,43 @@
 (in-package :cl-user)
 (defpackage darkmatter/notebook/extentions/ipynb
   (:use :cl)
-  (:export :serialize.ipynb
+  (:import-from :darkmatter/notebook/domains/cell
+                :code-cell-entity
+                :code-cell-source
+                :code-cell-execution-count
+                :code-cell-collapsed
+                :code-cell-autoscroll
+                :code-cell-outputs
+                :make-note-cell-entity
+                :note-cell-entity
+                :note-cell-format
+                :note-cell-source
+                :make-raw-cell-entity
+                :raw-cell-entity
+                :raw-cell-format
+                :raw-cell-source
+                :make-stream-output-entity
+                :stream-output-entity
+                :stream-output-stream
+                :stream-output-source
+                :make-display-data-output-entity
+                :display-data-output-entity
+                :display-data-output-data
+                :display-data-output-metadata
+                :make-execute-result-output-entity
+                :execute-result-output-entity
+                :execute-result-output-execution-count
+                :execute-result-output-data
+                :execute-result-output-metadata
+                :make-error-output-entity
+                :error-output-entity
+                :error-output-name
+                :error-output-value
+                :error-output-traceback)
+  (:import-from :darkmatter/notebook/utils/convert
+                :plist-to-hash-table)
+  (:export :map-to-ipynb
+           :serialize.ipynb
            :deserialize.ipynb))
 (in-package :darkmatter/notebook/extentions/ipynb)
 
@@ -46,17 +82,29 @@
       (yason:encode-object-element "outputs"
         (ipynb.code-cell-outputs object)))))
 
+(defstruct (ipynb.markdown-cell (:include ipynb.cell)))
+
+(defmethod yason:encode ((object ipynb.markdown-cell) &optional (stream *standard-output*))
+  (yason:with-output (stream)
+    (yason:with-object ()
+      (yason:encode-object-element "cell_type"
+        (ipynb.markdown-cell-cell-type object))
+      (yason:encode-object-element "metadata"
+        (ipynb.markdown-cell-metadata object))
+      (yason:encode-object-element "source"
+        (ipynb.markdown-cell-source object)))))
+
 (defstruct (ipynb.raw-cell (:include ipynb.cell)))
 
 (defmethod yason:encode ((object ipynb.raw-cell) &optional (stream *standard-output*))
   (yason:with-output (stream)
     (yason:with-object ()
       (yason:encode-object-element "cell_type"
-        (ipynb.cell-cell-type object))
+        (ipynb.raw-cell-cell-type object))
       (yason:encode-object-element "metadata"
-        (ipynb.cell-metadata object))
+        (ipynb.raw-cell-metadata object))
       (yason:encode-object-element "source"
-        (ipynb.cell-source object)))))
+        (ipynb.raw-cell-source object)))))
 
 (deftype %ipynb.code-cell-output-type () 'string)
 (defstruct ipynb.code-cell-output
@@ -164,13 +212,109 @@
                                        ki)
           (gethash "language_info" ht) (let ((li (make-hash-table :test #'equal)))
                                          (setf (gethash "name" li) "common-lisp"
-                                               (gethash "version" li) "1.0"
+                                               (gethash "version" li) "X3J13"
                                                (gethash "codemirror_mode" li) "commonlisp")
                                          li))
     ht))
 
+(defun %make-ipynb-code-cell.metadata (collapsed autoscroll)
+  (let ((ht (make-hash-table :test #'equal)))
+    (setf (gethash "collapsed" ht)
+          (if collapsed 'yason:true 'yason:false)
+          (gethash "autoscroll" ht)
+          (if autoscroll 'yason:true 'yason:false))
+    ht))
+
+(defun %make-ipynb-stream-output (output)
+  (make-ipynb.stream-output
+    :output-type "stream"
+    :name (case (stream-output-stream output)
+            (:stdout "stdout")
+            (:stderr "stderr"))
+    :text (stream-output-source output)))
+
+(defun %make-ipynb-display-data (output)
+  (make-ipynb.display-data
+    :output-type "display_data"
+    :data (plist-to-hash-table (display-data-output-data output))
+    :metadata (plist-to-hash-table (display-data-output-metadata output))))
+
+(defun %make-ipynb-execute-result (output)
+  (make-ipynb.execute-result
+    :output-type "execute_result"
+    :execution-count (execute-result-output-execution-count output)
+    :data (plist-to-hash-table (execute-result-output-data output))
+    :metadata (plist-to-hash-table (execute-result-output-metadata output))))
+
+(defun %make-ipynb-error-output (output)
+  (make-ipynb.error-output
+    :output-type "error"
+    :ename (error-output-name output)
+    :evalue (error-output-value output)
+    :traceback (error-output-traceback output)))
+
+(defun %map-to-ipynb-code-cell-output (output)
+  (typecase output
+    (stream-output-entity
+      (%make-ipynb-stream-output output))
+    (display-data-output-entity
+      (%make-ipynb-display-data output))
+    (execute-result-output-entity
+      (%make-ipynb-execute-result output))
+    (error-output-entity
+      (%make-ipynb-error-output output))))
+
 (defun %convert-to-ipynb-code-cell (cell)
-  ()) ;; TODO
+  (make-ipynb.code-cell
+    :cell-type "code"
+    :metadata (%make-ipynb-code-cell.metadata
+                (code-cell-collapsed cell)
+                (code-cell-autoscroll cell))
+    :source (code-cell-source cell)
+    :execution-count (code-cell-execution-count cell)
+    :outputs (mapcar #'%map-to-ipynb-code-cell-output (code-cell-outputs cell))))
+
+(defun %convert-to-ipynb-markdown-cell (cell)
+  (make-ipynb.markdown-cell
+    :cell-type "markdown"
+    :metadata (make-hash-table :test #'equal)
+    :source (note-cell-source cell)))
+
+(defun %make-ipynb-raw-cell.metadata-for-asciidoc ()
+  (let ((ht (make-hash-table :test #'equal)))
+    (setf (gethash "format" ht)
+          "text/asciidoc")
+    ht))
+
+(defun %convert-to-ipynb-raw-cell-from-asciidoc (cell)
+  (make-ipynb.raw-cell
+    :cell-type "raw"
+    :metadata (%make-ipynb-raw-cell.metadata-for-asciidoc)
+    :source (note-cell-source cell)))
+
+(defun %make-ipynb-raw-cell.metadata-for-latex ()
+  (let ((ht (make-hash-table :test #'equal)))
+    (setf (gethash "format" ht)
+          "text/html")
+    ht))
+
+(defun %convert-to-ipynb-raw-cell-from-latex (cell)
+  (make-ipynb.raw-cell
+    :cell-type "raw"
+    :metadata (%make-ipynb-raw-cell.metadata-for-latex)
+    :source (note-cell-source cell)))
+
+(defun %make-ipynb-raw-cell.metadata (format)
+  (let ((ht (make-hash-table :test #'equal)))
+    (setf (gethash "format" ht)
+          format)
+    ht))
+
+(defun %convert-to-ipynb-raw-cell (cell)
+  (make-ipynb.raw-cell
+    :cell-type "raw"
+    :metadata (%make-ipynb-raw-cell.metadata (raw-cell-format cell))
+    :source (raw-cell-source cell)))
 
 (defun %map-to-ipynb-cell (cell)
   (typecase cell
@@ -190,10 +334,12 @@
 (defun map-to-ipynb (cells)
   (make-ipynb-format
     :metadata (%make-ipynb-format.metadata)
-    :cells (map)))
+    :cells (mapcar #'%map-to-ipynb-cell cells)))
 
-(defun serialize.ipynb ()
-  )
+(defun serialize.ipynb (ipynb)
+  (let ((stream (make-string-output-stream)))
+    (yason:encode ipynb stream)
+    (get-output-stream-string stream)))
 
 (defun deserialize.ipynb ()
   )
