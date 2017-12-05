55baa7c7a0c264a5c7178d813ba5964749986ab6
tamamu <tamamu.1r1s@gmail.com>
2017-12-06 00:38:55 +0900

    Add hash-table-to-plist

--- src/utils/convert.lisp
+++ src/utils/convert.lisp
@@ -1,7 +1,8 @@
 (in-package :cl-user)
 (defpackage darkmatter/notebook/utils/convert
   (:use :cl)
-  (:export :plist-to-hash-table))
+  (:export :plist-to-hash-table
+           :hash-table-to-plist))
 (in-package :darkmatter/notebook/utils/convert)
 
 (defun plist-to-hash-table (plist)
@@ -9,3 +10,11 @@
     (loop for (key value) on plist by #'cddr do
           (setf (gethash key ht) value))
     ht))
+
+(defun hash-table-to-plist (ht)
+  (let ((plist (list)))
+    (maphash
+      (lambda (key val)
+        (setf plist (append plist (list key val))))
+      ht)
+    plist))
