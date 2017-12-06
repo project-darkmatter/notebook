131ab93a009c57f34e42385b8416aecc9d3a0feb
tamamu <tamamu.1r1s@gmail.com>
2017-12-07 00:42:08 +0900

    Support .ipynb format

--- src/extention.lisp
+++ src/extention.lisp
@@ -1,8 +1,23 @@
 (in-package :cl-user)
 (defpackage darkmatter/notebook/extention
   (:use :cl)
+  (:import-from :darkmatter/notebook/utils/string-case
+                :string-case)
+  (:import-from :darkmatter/notebook/utils/file
+                :read-file-into-string)
+  (:import-from :darkmatter/notebook/extentions/ipynb
+                :deserialize.ipynb
+                :convert-to-cells-from-ipynb)
   (:export :read-as-notebook))
 (in-package :darkmatter/notebook/extention)
 
+(defun %read-as-ipynb (pathname)
+  (let ((source (read-file-into-string pathname)))
+    (convert-to-cells-from-ipynb
+      (deserialize.ipynb source))))
+
 (defun read-as-notebook (pathname)
-  nil)
+  (let ((extention (string-upcase (pathname-type pathname))))
+    (string-case extention
+      ("IPYNB" (%read-as-ipynb pathname))
+      (t nil))))
