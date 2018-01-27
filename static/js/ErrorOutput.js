be15ee326bf51b0c22b479d6dbe88ad6a18c4ae2
tamamu <tamamu.1r1s@gmail.com>
2018-01-28 02:22:08 +0900

    Add ErrorOutput class

---
+++ static/js/ErrorOutput.js
@@ -0,0 +1,29 @@
+
+class ErrorOutput {
+  constructor(base, parent = null) {
+    this.element = ErrorOutput.__container__();
+    switch (typeof base) {
+      case "string":
+        this.traceback = base;
+        parent.appendChild(this.element);
+        break;
+      case "object":
+        this.traceback = base.innerText;
+        base.parentNode.replaceChild(this.element, base);
+        break;
+    }
+    let nodes = ErrorOutput.__traceback__(this.traceback);
+    for (let node of nodes) {
+      this.element.appendChild(node);
+    }
+  }
+
+  static __container__() {
+    let elm = document.createElement("div");
+    return elm;
+  }
+
+  static __traceback__(text) {
+    return CSIToHTML(text);
+  }
+}
